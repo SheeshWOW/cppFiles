b831382f734c973c7690b382825eb742e64ac569
SnerMorY <Kabumzaharov@yandex.ru>
2024-07-20 11:37:10 +0300

    First commit

---
+++ main.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <ctime>
+#include <windows.h>
+
+
+using namespace std;
+
+void FillArray(int* const arr, const int size);
+void DisplayArray(const int* const arr, const int size);
+
+void push_back(int*& arr, int& size, const int value);
+
+int main(){
+   srand(time(NULL));
+
+   int size = 2;
+   int* arr = new int[size];
+
+    FillArray(arr, size);
+    DisplayArray(arr, size);
+
+    for (int i = 0; i < 5; i++) {
+        push_back(arr, size, rand()%20+1);
+        DisplayArray(arr, size);
+    }
+    
+    return 0;
+}
+
+void FillArray(int* const arr, const int size) {
+    for (int i=0; i<size; i++) {
+        arr[i] = rand() % 50 + 1;
+    }
+}
+
+void DisplayArray(const int* const arr, const int size) {
+    cout << '[';
+    for (int i=0; i<size; i++) {
+        cout << arr[i];
+        if (i+1 != size) {
+            cout << ", ";
+        }
+    }
+    cout << ']' << endl;
+}
+
+void push_back(int*& arr, int& size, const int value) {
+    int* newArray = new int[size+1];
+
+    for (int i=0; i<size; i++) {
+        newArray[i] = arr[i];
+    }
+
+    newArray[size] = value;
+
+    size++;
+
+    delete[] arr;
+
+    arr = newArray;
+}
+
+
